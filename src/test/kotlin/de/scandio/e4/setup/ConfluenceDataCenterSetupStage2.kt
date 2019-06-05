58cfb65500ce3f9bc526982e8cae3c636f6124be
Felix Grund <felix.grund@scandio.de>
2019-06-04 21:48:19 -0700

    Uncommented stage 2 steps

--- src/test/kotlin/de/scandio/e4/setup/ConfluenceDataCenterSetupStage2.kt
+++ src/test/kotlin/de/scandio/e4/setup/ConfluenceDataCenterSetupStage2.kt
@@ -33,38 +33,38 @@ class ConfluenceDataCenterSetupStage2 : SetupBaseTest() {
     @Test
     fun test() {
         try {
-//            webConfluence.navigateTo("setup/setupdata-start.action")
-//            dom.awaitSeconds(3) // just wait a bit for safety
-//
-//            /* Step 6: Setup data */
-//            dom.click("input[Value='Empty Site']")
-//
-//            /* Step 7: User management */
-//            dom.click("#internal")
-//
-//            /* Step 8: Admin account */
-//
-//            dom.insertText("#fullName", "Administrator")
-//            dom.insertText("#email", "admin@example.com")
-//            dom.insertText("#password", "admin")
-//            dom.insertText("#confirm", "admin")
-//            dom.click("#setup-next-button")
-//
-//
-//            driver.findElement(By.linkText("Start")).click()
-//
-//            dom.insertText("#grow-intro-space-name", "TEST")
-//            dom.click("#grow-intro-create-space")
-//            dom.click("#onboarding-skip-editor-tutorial")
-//            dom.click("#editor-precursor > .cell")
-//            dom.click("#content-title")
-//            dom.insertText("#content-title", "Test Page")
-//            dom.click("#rte-button-publish")
-//            dom.awaitElementPresent("#main-content")
+            webConfluence.navigateTo("setup/setupdata-start.action")
+            dom.awaitSeconds(3) // just wait a bit for safety
+
+            /* Step 6: Setup data */
+            dom.click("input[Value='Empty Site']")
+
+            // Step 7: User management */
+            dom.click("#internal")
+
+            /* Step 8: Admin account */
+
+            dom.insertText("#fullName", "Administrator")
+            dom.insertText("#email", "admin@example.com")
+            dom.insertText("#password", "admin")
+            dom.insertText("#confirm", "admin")
+            dom.click("#setup-next-button")
+
+
+            driver.findElement(By.linkText("Start")).click()
+
+            dom.insertText("#grow-intro-space-name", "TEST")
+            dom.click("#grow-intro-create-space")
+            dom.click("#onboarding-skip-editor-tutorial")
+            dom.click("#editor-precursor > .cell")
+            dom.click("#content-title")
+            dom.insertText("#content-title", "Test Page")
+            dom.click("#rte-button-publish")
+            dom.awaitElementPresent("#main-content")
 
             /* Step 9: Admin config */
-//            webConfluence.disableSecureAdminSessions()
-//            webConfluence.disableCaptchas()
+            webConfluence.disableSecureAdminSessions()
+            webConfluence.disableCaptchas()
             webConfluence.disablePlugin("com.atlassian.troubleshooting.plugin-confluence")
             webConfluence.setLogLevel("co.goodsoftware", "INFO")
             webConfluence.installPlugin("$IN_DIR/$DATA_GENERATOR_JAR_FILENAME")
