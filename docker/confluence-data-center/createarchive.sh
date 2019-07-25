f77a2a04d76bc9f9854491156592d6cf4c3d1a44
Felix Grund <felix.grund@scandio.de>
2019-07-25 23:04:16 +0200

    createarchive changes

--- docker/confluence-data-center/createarchive.sh
+++ docker/confluence-data-center/createarchive.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+  echo "Usage: createarchive.sh NAME"
+  exit 0
+fi
+mkdir $1
+mkdir $1/confluence-home
+mkdir $1/confluence-shared-home
 
-mkdir archive
-mkdir archive/confluence-home
-mkdir archive/confluence-shared-home
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/confluence.cfg.xml $1/confluence-home/
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/index $1/confluence-home/
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/journal $1/confluence-home/
 
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/confluence.cfg.xml archive/confluence-home/
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/index archive/confluence-home/
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-home/journal archive/confluence-home/
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/confluence.cfg.xml $1/confluence-shared-home/
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/attachments $1/confluence-shared-home/
+docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/config $1/confluence-shared-home/
 
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/confluence.cfg.xml archive/confluence-shared-home/
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/attachments archive/confluence-shared-home/
-docker cp $(docker ps -qf "name=confluence-cluster-6153-node1"):/confluence-shared-home/config archive/confluence-shared-home/
+docker exec $(docker ps -qf "name=confluence-cluster-6153-db") pg_dump -U confluence -Fc confluence > $1/confluencedb.tar.gz
 
-docker exec $(docker ps -qf "name=confluence-cluster-6153-db") pg_dump -U confluence -Fc confluence > archive/confluencedb.tar.gz
+tar cf $1.tar.gz $1
 
-tar cf $1.tar.gz archive/*
-
-rm -r archive
+rm -r $1
