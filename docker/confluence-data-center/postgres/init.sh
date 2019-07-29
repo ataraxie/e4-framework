a612287c5b26ae07af49a5bc8175ce8df9eb102e
Felix Grund <felix.grund@scandio.de>
2019-07-29 13:36:39 +0200

    Work on cluster script

--- docker/confluence-data-center/postgres/init.sh
+++ docker/confluence-data-center/postgres/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Given by --env: $E4_PROV_KEY
+# Given by --env: $E4_PROV_KEY, $E4_PLATFORM_NAME
 
-if [ -f /e4prov/$E4_PROV_KEY/confluencedb.tar.gz ];
+if [ -f /e4prov/$E4_PROV_KEY/${E4_APP_NAME}db.tar.gz ];
 then
-  echo ">>> E4 postgres-init: Dump file found. Restore: /e4prov/$E4_PROV_KEY/confluencedb.tar.gz"
+  echo ">>> E4 postgres-init: Dump file found. Restore: /e4prov/$E4_PROV_KEY/${E4_APP_NAME}db.tar.gz"
   START=$(date +%s)
-  pg_restore -U confluence -j 8 -d confluence /e4prov/$E4_PROV_KEY/confluencedb.tar.gz
+  pg_restore -U $E4_APP_NAME -j 8 -d $E4_APP_NAME /e4prov/$E4_PROV_KEY/${E4_APP_NAME}db.tar.gz
   END=$(date +%s)
   echo ">>> Time taken for restore: $(($END - $START)) seconds"
 else
