3328ef87e29eae730ab926f39572ce06a74068c4
Felix Grund <felix.grund@scandio.de>
2019-09-16 15:50:53 +0200

    Minor script fix

--- docker/atlassian-cluster/e4-atlassian-cluster.sh
+++ docker/atlassian-cluster/e4-atlassian-cluster.sh
@@ -475,7 +475,11 @@ then
 	else
 		echo ">> Provision resources found for $E4_APP_NAME_UCASE $E4_APP_VERSION with key $E4_PROV_KEY"
 	fi
-	chmod -R 777 $E4_PROV_DIR/$E4_PROV_KEY # FIXME: sometimes we run into file permission issues and I haven't found a way to fix it yet
+
+	if [[ -d $E4_PROV_DIR/$E4_PROV_KEY ]];
+	then
+		chmod -R 777 $E4_PROV_DIR/$E4_PROV_KEY # FIXME: sometimes we run into file permission issues and I haven't found a way to fix it yet
+	fi
 
 	create_network
 	echo ""
