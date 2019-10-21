6e69397c7afc67cd0a68b217181e68481b62bf04
Felix Grund <felix.grund@scandio.de>
2019-10-21 19:13:26 +0200

    Git Snippets fix

--- src/main/kotlin/de/scandio/e4/testpackages/gitsnippets/actions/SetupGitSnippets.kt
+++ src/main/kotlin/de/scandio/e4/testpackages/gitsnippets/actions/SetupGitSnippets.kt
@@ -6,6 +6,7 @@ import de.scandio.e4.worker.interfaces.WebClient
 import de.scandio.e4.clients.rest.RestConfluence
 import de.scandio.e4.clients.web.WebConfluence
 import de.scandio.e4.worker.util.RandomData
+import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 import java.util.*
 
@@ -14,10 +15,17 @@ class SetupGitSnippets: Action() {
     protected var start: Long = 0
     protected var end: Long = 0
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun execute(webClient: WebClient, restClient: RestClient) {
         val webConfluence = webClient as WebConfluence
         val dom = webConfluence.dom
-        val accessToken = "60578a45881a97098af808b9198830383b799b2d"
+        val accessToken = System.getenv("GIT_SNIPPETS_TOKEN") ?: throw Exception("Access Token not found!")
+        if (StringUtils.isNotBlank(accessToken)) {
+            log.warn("SUCCESS: TOKEN!")
+        } else {
+            log.warn("ERROR: TOKEN!")
+        }
         webConfluence.login()
         this.start = Date().time
         webConfluence.navigateTo("admin/plugins/git-snippets/settings.action")
