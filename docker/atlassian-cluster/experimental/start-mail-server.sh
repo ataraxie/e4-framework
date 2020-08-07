5310e80bdc47cb1247c07912946304f6126d1f8c
fgrund <felix@livelyapps.com>
2020-08-07 07:36:03 -0700

    Change mail server config

--- docker/atlassian-cluster/experimental/start-mail-server.sh
+++ docker/atlassian-cluster/experimental/start-mail-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run \
-  --net=confluence-cluster-701 \
-  --net-alias=confluence-cluster-701-mail \
+  --net=confluence \
+  --net-alias=confluence-mail \
   --name mailcatcher \
   -p 1080:1080 -p 1025:1025 \
   -d schickling/mailcatcher
